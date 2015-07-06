f6f5b184a7136365617954596cd6930a7a82c40f
gclen <graham.clendenning@uoit.net>
2015-07-06 11:59:25 -0400

    Added Davids changes

--- gaussian_config.py
+++ gaussian_config.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
-
+ 
 import glob
-
+from StringIO import StringIO
+ 
 def parse_route_section(gaussian_input_file,keywords):
-
+ 
     prev_line=''
-
+ 
     input_file=open(gaussian_input_file,'r')
-
+ 
     for line in input_file:
         #Look for route section
         if line[0]=='#':
             route_section=line.split()
-            
+           
             for option in route_section:
                 #Relaxation keyword
                 if option=='opt':
@@ -24,7 +25,7 @@ def parse_route_section(gaussian_input_file,keywords):
                     #Put the functional in the keyword list
                     keywords.append(keyword_split[0])
                     #Check the basis set
-                    #If there are different basis functions per atom 
+                    #If there are different basis functions per atom
                     #They are at the bottom of the file
                     if keyword_split[1].lower()=='gen':
                         pass
@@ -35,55 +36,54 @@ def parse_route_section(gaussian_input_file,keywords):
                 if 'SCRF=' in option:
                     keyword_split=option.split('=')
                     keywords.append(keyword_split[-1].rstrip(')'))
-                if 'pop=' in option: 
+                #Check for population analysis
+                if 'pop=' in option:
                     keyword_split=option.split('=')
                     keywords.append('Population analysis')    
+                #Check for TDDFT
                 if ('td(' or 'td=') in option:
                     keywords.append('TD-DFT')
+                #Check for frequency of calclations
                 if 'freq' in option:
                     keywords.append('Frequency')
         #For per atom basis sets, the name is in the line previous to ****
         if line.rstrip()=='****':
             keywords.append(prev_line.rstrip())
-            
+           
         prev_line=line
-
+ 
     return keywords
-
+ 
 def write_config(keywords,files):
-
-    config_file=open('addConfig.txt','w')
-
+    config_file = StringIO()
+ 
     config_file.write('Description\n\n')
     config_file.write('Keywords\n')
-    
+   
     for keyword in keywords:
         config_file.write('    '+keyword+'\n')
-    
+   
     config_file.write('\ninputfiles\n')
-    
+   
     for file_name in files:
         config_file.write('    '+file_name+'\n')
-    
-    config_file.write('\noutputfiles\n')
-
-    config_file.close()
-
-if __name__=="__main__":
-
+ 
+    return config_file.getvalue()
+ 
+def run():
     keywords_list=[]
     file_list=[]
-
+ 
     for g09_input in glob.glob("*.gjf"):
         keywords_list=parse_route_section(g09_input,keywords_list)
         file_list.append(g09_input)
-    
+   
     #Remove duplicate keywords
     keywords_list=list(set(keywords_list))
-    
-    write_config(keywords_list,file_list)    
-    
-
+   
+    return write_config(keywords_list,file_list)
+   
+run()
 
 
 
